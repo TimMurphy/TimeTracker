8ac2ecaa63df285d96a993d24e3d678548829d94
Tim Murphy <tim@26tp.com>
2014-08-23 06:23:42 +1000

    Fixed tests to start IIS Express when required.

--- src/TimeTracker.Specifications/Support/WebApplication/IISExpressWebServer.cs
+++ src/TimeTracker.Specifications/Support/WebApplication/IISExpressWebServer.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 
 namespace TimeTracker.Specifications.Support.WebApplication
 {
     public class IISExpressWebServer : IWebServer
     {
+        private readonly string BaseUrl;
         private readonly string IISExpressExe;
+        private readonly string IndexPageTitle;
         private readonly string WebsiteName;
 
         private Process Process;
 
-        public IISExpressWebServer(string websiteName)
-            : this(GetIISExpressExe(), websiteName)
+        public IISExpressWebServer(string websiteName, string baseUrl, string indexPageTitle)
+            : this(GetIISExpressExe(), websiteName, baseUrl, indexPageTitle)
         {
         }
 
-        public IISExpressWebServer(string iisExpressExe, string websiteName)
+        public IISExpressWebServer(string iisExpressExe, string websiteName, string baseUrl, string indexPageTitle)
         {
             if (!File.Exists(iisExpressExe))
             {
@@ -25,6 +28,8 @@ namespace TimeTracker.Specifications.Support.WebApplication
 
             IISExpressExe = iisExpressExe;
             WebsiteName = websiteName;
+            BaseUrl = baseUrl;
+            IndexPageTitle = indexPageTitle;
         }
 
         public void StartIfNotRunning()
@@ -57,12 +62,30 @@ namespace TimeTracker.Specifications.Support.WebApplication
 
         private bool IsRunning()
         {
-            // todo: test via ping?
-            return Process != null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+
+                var response = client.GetAsync("/").Result;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return false;
+                }
+
+                var content = response.Content.ReadAsStringAsync().Result;
+
+                return content.Contains(string.Format("<title>{0}</title>", IndexPageTitle));
+            }
         }
 
         private void Stop()
         {
+            if (Process == null)
+            {
+                return;
+            }
+
             if (!Process.HasExited)
             {
                 Process.Kill();
--- src/TimeTracker.Specifications/Support/WebApplication/WebApplicationManager.cs
+++ src/TimeTracker.Specifications/Support/WebApplication/WebApplicationManager.cs
@@ -5,25 +5,21 @@ namespace TimeTracker.Specifications.Support.WebApplication
 {
     public class WebApplicationManager : IWebApplicationManager
     {
+        private const string WebsiteName = "TimeTracker.WebApplication"; // todo: get website name from project file?
+        private const string IndexPageTitle = "TimeTracker";
+
         public WebApplicationManager()
             : this(GetUrl("http"), GetUrl("https"))
         {
         }
 
-        private static string GetUrl(string protocol)
-        {
-            return string.Format("{0}://{1}/", protocol, Environment.MachineName);
-        }
-
         public WebApplicationManager(string httpUrl, string httpsUrl)
-            : this(
-                // todo: get website name from project file?
-                "TimeTracker.WebApplication", httpUrl, httpsUrl)
+            : this(WebsiteName, httpUrl, httpsUrl, IndexPageTitle)
         {
         }
 
-        public WebApplicationManager(string websiteName, string httpUrl, string httpsUrl)
-            : this(new IISExpressWebServer(websiteName), new BaseUri(httpUrl, httpsUrl))
+        public WebApplicationManager(string websiteName, string httpUrl, string httpsUrl, string indexPageTitle)
+            : this(new IISExpressWebServer(websiteName, httpUrl, indexPageTitle), new BaseUri(httpUrl, httpsUrl))
         {
         }
 
@@ -35,5 +31,10 @@ namespace TimeTracker.Specifications.Support.WebApplication
 
         public BaseUri BaseUri { get; private set; }
         public IWebServer WebServer { get; private set; }
+
+        private static string GetUrl(string protocol)
+        {
+            return string.Format("{0}://{1}/", protocol, Environment.MachineName);
+        }
     }
 }
