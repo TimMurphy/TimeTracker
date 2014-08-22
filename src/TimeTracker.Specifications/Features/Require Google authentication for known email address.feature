221737454a8b9ab4df4224e05c3242070f1aad5c
Tim Murphy <tim@26tp.com>
2014-08-23 06:45:04 +1000

    Added 'Require Google authentication for known email address' specification.

---
+++ src/TimeTracker.Specifications/Features/Require Google authentication for known email address.feature
@@ -0,0 +1,21 @@
+﻿Feature: Require Google authentication for known email address
+	In order to keep my time tracking secure
+	As a known user
+	I want all pages to be require my Google account
+
+Scenario: Not authenticated
+	Given I am not authenticated
+	When I goto any url
+	Then I am redirected to the sign in page
+
+Scenario: Authenticated with known email address
+	Given I am authenticated with a known email addresses
+	When I goto any url
+	Then I can view the requested page
+
+Scenario: Authenticated with unknown email address
+	Given I am authenticated with an unknown email addresses
+	When I goto any url
+	Then I am redirected to the sign in page
+	And the sign in page says I am an unknown user
+
---
+++ src/TimeTracker.Specifications/Features/Steps/RequireGoogleAuthenticationForKnownEmailAddressSteps.cs
@@ -0,0 +1,51 @@
+﻿using TechTalk.SpecFlow;
+using TimeTracker.Specifications.Support;
+
+namespace TimeTracker.Specifications.Features.Steps
+{
+    [Binding]
+    public class RequireGoogleAuthenticationForKnownEmailAddressSteps : BaseSteps
+    {
+        [Given(@"I am not authenticated")]
+        public void GivenIAmNotAuthenticated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I am authenticated with a known email addresses")]
+        public void GivenIAmAuthenticatedWithAKnownEmailAddresses()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I am authenticated with an unknown email addresses")]
+        public void GivenIAmAuthenticatedWithAnUnknownEmailAddresses()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I goto any url")]
+        public void WhenIGotoAnyUrl()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I am redirected to the sign in page")]
+        public void ThenIAmRedirectedToTheSignInPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I can view the requested page")]
+        public void ThenICanViewTheRequestedPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the sign in page says I am an unknown user")]
+        public void ThenTheSignInPageSaysIAmAnUnknownUser()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
