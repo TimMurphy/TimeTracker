4409248c69b348170a48df2b7b01a32e1d2bd83b
Tim Murphy <tim@26tp.com>
2014-08-27 06:29:35 +1000

    Added specification for EventQueue.AddAsync(events).

---
+++ src/TimeTracker.UnitTests/Domain.Infrastructure/EventQueue.feature
@@ -0,0 +1,9 @@
+﻿Feature: EventQueue
+
+Background: 
+	Given an EventQueue
+
+Scenario: AddAsync(events)
+	Given I a collection of events
+	When I call AddAsync(events)
+	Then the events are added to the queue
---
+++ src/TimeTracker.UnitTests/Domain.Infrastructure/Steps/EventQueueSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace TimeTracker.UnitTests.Domain.Infrastructure.Steps
+{
+    [Binding]
+    public class EventQueueSteps
+    {
+        [Given(@"an EventQueue")]
+        public void GivenAnEventQueue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I a collection of events")]
+        public void Given_I_A_CollectionOfEvents()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I call AddAsync\(events\)")]
+        public void WhenICallAddAsyncEvents()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the events are added to the queue")]
+        public void ThenTheEventsAreAddedToTheQueue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
